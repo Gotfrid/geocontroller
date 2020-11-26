51f6182526b4eddf6ab1730852c1ccc06033e58c
Pavel <paveldemin@me.com>
2020-11-26 20:01:04 +0300

    style fix + gitignore

--- scripts/01_prepare_data.R
+++ scripts/01_prepare_data.R
@@ -1,20 +1,26 @@
 # list all downloaded shp files
-shp_files = list.files(path = "data", pattern = "\\.shp$", 
-                       recursive = T, full.names = T)
+shp_files <- list.files(
+    path = "data", pattern = "\\.shp$",
+    recursive = T, full.names = T
+)
 
 # get only places as points
-places_points = shp_files[grepl(pattern = "places_free", x = shp_files)] # omit places_a which is polygons
+places_points <- shp_files[grepl(pattern = "places_free", x = shp_files)] # omit places_a which is polygons
 
 # load this into R
 # TODO do it in more eddicient way
-all_places_points = st_sfc(crs = 4326)
+all_places_points <- st_sfc(crs = 4326)
 for (i in seq_along(places_points)) {
-    tmp_places_points = sf::read_sf(places_points[i])
-    tmp_places_points = subset(x = tmp_places_points, 
-                               subset = !fclass %in% c("farm", "locality", "suburb"))
-    all_places_points = rbind(all_places_points, tmp_places_points)
+    tmp_places_points <- sf::read_sf(places_points[i])
+    tmp_places_points <- subset(
+        x = tmp_places_points,
+        subset = !fclass %in% c("farm", "locality", "suburb")
+    )
+    all_places_points <- rbind(all_places_points, tmp_places_points)
 }
 
 # save result file as RDS - very efficient R way to store data
-saveRDS(object = all_places_points,
-        file = "data/all_places_points.rds")
+saveRDS(
+    object = all_places_points,
+    file = "data/all_places_points.rds"
+)
